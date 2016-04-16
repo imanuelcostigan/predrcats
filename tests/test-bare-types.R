2b9a56d022dfad55eed2fc74420586b8806ebe61
Imanuel Costigan <i.costigan@me.com>
2016-04-17 08:33:27 +1000

    Add bare type test cases

---
+++ tests/test-bare-types.R
@@ -0,0 +1,13 @@
+context("Bare types")
+
+test_that("Bare type predicates work", {
+  expect_true(is_list(data.frame(a = 1)))
+  expect_false(is_bare_list(data.frame(a = 1)))
+  expect_false(is_bare_list(list(a = 1)))
+  expect_true(is_double(Sys.Date()))
+  expect_false(is_bare_double(Sys.Date()))
+  expect_true(is_bare_double(1))
+  expect_true(is_bare_integer(as.matrix(1:10)))
+  expect_false(is_bare_integer(matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3,
+    byrow = TRUE, dimnames = list(c("row1", "row2"), c("C.1", "C.2", "C.3")))))
+})
